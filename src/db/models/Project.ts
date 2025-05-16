b1ce7d01e85992ca0076c88b3bfdcd4cc90ce170
Chloé <michelchloe04@gmail.com>
2025-05-16 15:55:59 +0200

    formats des datas step3

--- src/db/models/Project.ts
+++ src/db/models/Project.ts
@@ -9,8 +9,40 @@ const ProjectSchema = new Schema<IProject>({
   description: {
     type: String,
     required: false
-  }
-
+  },
+  step2: {
+    features: [
+      {
+        relatedTo: {
+          type: Schema.Types.ObjectId,
+          ref: 'Feature'
+        }
+      }
+    ]
+  },
+  step3: [
+    {
+      id: {
+        type: String,
+        required: true
+      },
+      titreSpec: { type: String, required: true },
+      contexte: { type: String, required: true },
+      objectifs: { type: String, required: true },
+      acteurs: { type: String, required: true },
+      description: { type: String, required: true },
+      conditionsSucces: { type: String, required: true },
+      preConditions: { type: String, required: true },
+      etapesFlux: { type: String, required: true },
+      scenariosErreurs: { type: String, required: true },
+      scenariosAlternatifs: { type: String, required: true },
+      reglesGestion: { type: String, required: true },
+      interfaceUxUi: { type: String, required: true },
+      casTests: { type: String, required: true },
+      postCondition: { type: String, required: true },
+      status: { type: String, required: true }
+    }
+  ]
 }, {
   timestamps: true
 })
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,7 +1,29 @@
-export interface IProject {
-  _id?: string
-  title: string
+export interface ISpecification {
+  id: string
+  titreSpec: string
+  contexte: string
+  objectifs: string
+  acteurs: string
   description: string
-  createdAt?: Date
-  updatedAt?: Date
+  conditionsSucces: string
+  preConditions: string
+  etapesFlux: string
+  scenariosErreurs: string
+  scenariosAlternatifs: string
+  reglesGestion: string
+  interfaceUxUi: string
+  casTests: string
+  postCondition: string
+  status: string
+}
+
+export interface IProject {
+  title: string
+  description?: string
+  step2: {
+    features: Array<{
+      relatedTo: string
+    }>
+  }
+  step3: ISpecification[]
 }
