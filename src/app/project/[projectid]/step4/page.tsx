647cb9520a6c44db62b9edc4936f0ff9f1fc6884
Yahlex <alexisremyh@gmail.com>
2025-06-12 14:28:49 +0200

    Update page.tsx step4 / 2

--- src/app/project/[projectid]/step4/page.tsx
+++ src/app/project/[projectid]/step4/page.tsx
@@ -1,8 +1,9 @@
 // app/project/[projectid]/step4/page.tsx
+
 import Step4Validation from '@/components/step4/step-4-validation'
 
 export default async function Step4Page ({ params }: { params: { projectid: string } }): Promise<React.ReactElement> {
-  const projectId = params.projectid
+  const projectId = await params.projectid
 
   return (
     <Step4Validation projectId={projectId} />
